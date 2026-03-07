b844e362e4f27978eb258c13ebf97ca36c303db4
anthonyleouu <antoleounakis@gmail.com>
2026-03-07 21:01:13 +0200

    Update tour.js limits

--- public/tour.js
+++ public/tour.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("TOUR JS VERSION 20260307-235");
+  console.log("TOUR JS VERSION 20260307-240");
 
   function state() {
     window.__VEIN_BOOKING__ = window.__VEIN_BOOKING__ || {};
